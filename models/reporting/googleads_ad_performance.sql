1ddf19a2ff9555c1dc9d805a794fde28f82167c2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-25 10:09:55 +0200

    Create googleads_ad_performance.sql

---
+++ models/reporting/googleads_ad_performance.sql
@@ -0,0 +1,57 @@
+{{ config (
+    alias = target.database + '_googleads_ad_performance'
+)}}
+
+WITH office_data as
+    (SELECT office as sf_office, 
+        case 
+            WHEN LEFT(office,1)='R' THEN SPLIT_PART(SPLIT_PART(office,' ',1),'R',2) 
+            WHEN LEFT(office,1)='B'THEN SPLIT_PART(office,' ',1)
+        end as code, 
+        SPLIT_PART(office,' ',2) + SPLIT_PART(office,' ',3) + SPLIT_PART(office,' ',4) as location
+    FROM {{ source('gsheet_raw', 'office_locations') }}
+    GROUP BY office
+    ORDER BY code ASC)
+  
+SELECT 
+account_id,
+ad_name,
+ad_id,
+ad_status,
+ad_group_name,
+ad_group_id,
+campaign_name,
+campaign_id,
+campaign_status,
+CASE WHEN campaign_name ~* 'Discovery' THEN 'Campaign Type: Discovery'
+    WHEN campaign_name ~* 'Performance Max' OR campaign_name ~* 'PMAX' THEN 'Campaign Type: Performance Max'
+    WHEN campaign_name ~* 'Branded' THEN 'Campaign Type: Branded'
+    WHEN campaign_name ~* 'metal roofing keywords' OR campaign_name ~* 'NBS evergreen keywords' THEN 'Campaign Type: Non Branded'
+    WHEN campaign_name ~* 'youtube' THEN 'Campaign Type: Youtube'
+    ELSE 'Campaign Type: Other'
+END as campaign_type_default,
+date,
+date_granularity,
+CASE WHEN account_id  = '4560674777' THEN 'Roofing'
+    WHEN account_id = '2819798401' THEN 'Basement'
+    END AS erie_type,
+CASE WHEN campaign_name ~* 'All Areas' THEN 'National'
+    ELSE 'Local'
+    END as market,
+spend,
+impressions,
+clicks,
+conversions as purchases,
+conversions_value as revenue,
+kashurbagetpricing as leads,
+video_views
+FROM {{ ref('googleads_performance_by_ad') }}
+LEFT JOIN (SELECT campaign_id, campaign_name, account_id, campaign_status, advertising_channel_type,  
+            case 
+                when account_id = '4560674777' THEN RIGHT(LEFT(campaign_name,4),3) 
+                when account_id = '2819798401' AND LEFT(campaign_name,4) = '0071' THEN 'B001'
+                when account_id = '2819798401' AND LEFT(campaign_name,4) = '0078' THEN 'B002'
+                when account_id = '2819798401' THEN LEFT(campaign_name,4)
+            end as code 
+                FROM {{ ref('googleads_campaigns') }}) USING(campaign_id, campaign_name, account_id, campaign_status)
+    LEFT JOIN office_data USING(code)
