5e7afe6e2a514164123657da29d50a1468b05343
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-25 10:01:42 +0200

    Create tiktok_ad_performance.sql

---
+++ models/reporting/tiktok_ad_performance.sql
@@ -0,0 +1,24 @@
+{{ config (
+    alias = target.database + '_tiktok_ad_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_status,
+campaign_type_default,
+adgroup_name,
+adgroup_id,
+adgroup_status,
+audience,
+ad_name,
+ad_id,
+ad_status,
+visual,
+date,
+date_granularity,
+cost as spend,
+impressions,
+clicks,
+form_submission_events as submit_form
+FROM {{ ref('erie_tiktok_performance_by_ad') }}
