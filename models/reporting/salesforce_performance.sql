9e20d89c2756ce9b65cb27ad4d3649af6bd4a853
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-25 15:56:06 +0200

    Update salesforce_performance.sql

--- models/reporting/salesforce_performance.sql
+++ models/reporting/salesforce_performance.sql
@@ -2,101 +2,133 @@
     alias = target.database + '_salesforce_performance'
 )}}
 
-WITH final_data as
+WITH office_data as
+    (SELECT office as sf_office, 
+        case 
+            WHEN LEFT(office,1)='R' THEN SPLIT_PART(SPLIT_PART(office,' ',1),'R',2) 
+            WHEN LEFT(office,1)='B'THEN SPLIT_PART(office,' ',1)
+        end as code, 
+        SPLIT_PART(office,' ',2) + SPLIT_PART(office,' ',3) + SPLIT_PART(office,' ',4) as location
+    FROM {{ source('gsheet_raw', 'office_locations') }}
+    GROUP BY office
+    ORDER BY code ASC),
+    
+    filetered_data as
     (SELECT *
     FROM {{ source('snowflake_superbolt','superbolt_daily_file') }}
-    WHERE (source = 'BPMX' OR _fivetran_deleted IS false))
+    WHERE (source = 'BPMX' OR _fivetran_deleted IS false)),
 
-SELECT DATE_TRUNC('day',lead_entry_date::date) as date, 'day' as date_granularity,
-    market, state, source, zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
-    COUNT(DISTINCT lead_id) as leads,
-    COALESCE(SUM(number_of_calls),0) as calls,
-    COALESCE(SUM("set"),0) as appointments,
-    COALESCE(SUM(demo),0) as demos,
-    COALESCE(SUM(hits),0) as hits,
-    COALESCE(SUM(issued),0) as issues,
-    COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
-    COALESCE(SUM(sold),0) as closed_deals,
-    COALESCE(SUM("gross__"),0) as gross,
-    COALESCE(SUM("net__"),0) as net,
-    COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
-    COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
-FROM final_data
-where source = 'BPMX'
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+    final_data as 
+    (SELECT DATE_TRUNC('day',lead_entry_date::date) as date, 'day' as date_granularity,
+        market, state, source, zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
+        COUNT(DISTINCT lead_id) as leads,
+        COALESCE(SUM(number_of_calls),0) as calls,
+        COALESCE(SUM("set"),0) as appointments,
+        COALESCE(SUM(demo),0) as demos,
+        COALESCE(SUM(hits),0) as hits,
+        COALESCE(SUM(issued),0) as issues,
+        COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
+        COALESCE(SUM(sold),0) as closed_deals,
+        COALESCE(SUM("gross__"),0) as gross,
+        COALESCE(SUM("net__"),0) as net,
+        COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
+        COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    FROM filetered_data
+    where source = 'BPMX'
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+    
+    UNION ALL
+    
+    SELECT DATE_TRUNC('week',lead_entry_date::date) as date, 'week' as date_granularity,
+        market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
+        COUNT(DISTINCT lead_id) as leads,
+        COALESCE(SUM(number_of_calls),0) as calls,
+        COALESCE(SUM("set"),0) as appointments,
+        COALESCE(SUM(demo),0) as demos,
+        COALESCE(SUM(hits),0) as hits,
+        COALESCE(SUM(issued),0) as issues,
+        COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
+        COALESCE(SUM(sold),0) as closed_deals,
+        COALESCE(SUM("gross__"),0) as gross,
+        COALESCE(SUM("net__"),0) as net,
+        COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
+        COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    FROM filetered_data
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+    
+    UNION ALL
+    
+    SELECT DATE_TRUNC('month',lead_entry_date::date) as date, 'month' as date_granularity,
+        market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
+        COUNT(DISTINCT lead_id) as leads,
+        COALESCE(SUM(number_of_calls),0) as calls,
+        COALESCE(SUM("set"),0) as appointments,
+        COALESCE(SUM(demo),0) as demos,
+        COALESCE(SUM(hits),0) as hits,
+        COALESCE(SUM(issued),0) as issues,
+        COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
+        COALESCE(SUM(sold),0) as closed_deals,
+        COALESCE(SUM("gross__"),0) as gross,
+        COALESCE(SUM("net__"),0) as net,
+        COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
+        COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    FROM filetered_data
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+    
+    UNION ALL
+    
+    SELECT DATE_TRUNC('quarter',lead_entry_date::date) as date, 'quarter' as date_granularity,
+        market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
+        COUNT(DISTINCT lead_id) as leads,
+        COALESCE(SUM(number_of_calls),0) as calls,
+        COALESCE(SUM("set"),0) as appointments,
+        COALESCE(SUM(demo),0) as demos,
+        COALESCE(SUM(hits),0) as hits,
+        COALESCE(SUM(issued),0) as issues,
+        COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
+        COALESCE(SUM(sold),0) as closed_deals,
+        COALESCE(SUM("gross__"),0) as gross,
+        COALESCE(SUM("net__"),0) as net,
+        COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
+        COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    FROM filetered_data
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+    
+    UNION ALL
+    
+    SELECT DATE_TRUNC('year',lead_entry_date::date) as date, 'year' as date_granularity,
+        market, state, source,zip, sub_source_id, sub_source, dispo, call_disposition, status_detail,
+        COUNT(DISTINCT lead_id) as leads,
+        COALESCE(SUM(number_of_calls),0) as calls,
+        COALESCE(SUM("set"),0) as appointments,
+        COALESCE(SUM(demo),0) as demos,
+        COALESCE(SUM(hits),0) as hits,
+        COALESCE(SUM(issued),0) as issues,
+        COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
+        COALESCE(SUM(sold),0) as closed_deals,
+        COALESCE(SUM("gross__"),0) as gross,
+        COALESCE(SUM("net__"),0) as net,
+        COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
+        COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    FROM filetered_data
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11)
 
-UNION ALL
-
-SELECT DATE_TRUNC('week',lead_entry_date::date) as date, 'week' as date_granularity,
-    market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
-    COUNT(DISTINCT lead_id) as leads,
-    COALESCE(SUM(number_of_calls),0) as calls,
-    COALESCE(SUM("set"),0) as appointments,
-    COALESCE(SUM(demo),0) as demos,
-    COALESCE(SUM(hits),0) as hits,
-    COALESCE(SUM(issued),0) as issues,
-    COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
-    COALESCE(SUM(sold),0) as closed_deals,
-    COALESCE(SUM("gross__"),0) as gross,
-    COALESCE(SUM("net__"),0) as net,
-    COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
-    COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
-FROM final_data
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
-
-UNION ALL
-
-SELECT DATE_TRUNC('month',lead_entry_date::date) as date, 'month' as date_granularity,
-    market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
-    COUNT(DISTINCT lead_id) as leads,
-    COALESCE(SUM(number_of_calls),0) as calls,
-    COALESCE(SUM("set"),0) as appointments,
-    COALESCE(SUM(demo),0) as demos,
-    COALESCE(SUM(hits),0) as hits,
-    COALESCE(SUM(issued),0) as issues,
-    COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
-    COALESCE(SUM(sold),0) as closed_deals,
-    COALESCE(SUM("gross__"),0) as gross,
-    COALESCE(SUM("net__"),0) as net,
-    COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
-    COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
-FROM final_data
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
-
-UNION ALL
-
-SELECT DATE_TRUNC('quarter',lead_entry_date::date) as date, 'quarter' as date_granularity,
-    market, state, source,zip,sub_source_id, sub_source, dispo, call_disposition, status_detail,
-    COUNT(DISTINCT lead_id) as leads,
-    COALESCE(SUM(number_of_calls),0) as calls,
-    COALESCE(SUM("set"),0) as appointments,
-    COALESCE(SUM(demo),0) as demos,
-    COALESCE(SUM(hits),0) as hits,
-    COALESCE(SUM(issued),0) as issues,
-    COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
-    COALESCE(SUM(sold),0) as closed_deals,
-    COALESCE(SUM("gross__"),0) as gross,
-    COALESCE(SUM("net__"),0) as net,
-    COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
-    COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
-FROM final_data
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
-
-UNION ALL
-
-SELECT DATE_TRUNC('year',lead_entry_date::date) as date, 'year' as date_granularity,
+SELECT 
+    date,
+    date_granularity,
     market, state, source,zip, sub_source_id, sub_source, dispo, call_disposition, status_detail,
-    COUNT(DISTINCT lead_id) as leads,
-    COALESCE(SUM(number_of_calls),0) as calls,
-    COALESCE(SUM("set"),0) as appointments,
-    COALESCE(SUM(demo),0) as demos,
-    COALESCE(SUM(hits),0) as hits,
-    COALESCE(SUM(issued),0) as issues,
-    COALESCE(SUM(CASE WHEN paid_out_date IS NOT NULL THEN 1 ELSE 0 END),0) as down_payments,
-    COALESCE(SUM(sold),0) as closed_deals,
-    COALESCE(SUM("gross__"),0) as gross,
-    COALESCE(SUM("net__"),0) as net,
-    COUNT(DISTINCT lead_id)-(COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Wrong Number' THEN lead_id END)+COUNT(DISTINCT CASE WHEN status_detail ~* 'Duplicate Record' THEN lead_id END)) as workable_leads,
-    COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads
+    location as office, 
+    sf_office as office_location,
+    calls,
+    appointments,
+    demos,
+    hits,
+    issues,
+    down_payments,
+    closed_deals,
+    gross,
+    net,
+    workable_leads,
+    ooa_leads
 FROM final_data
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+LEFT JOIN office_data ON final_data.market = office_data.sf_office 
