2add4b52dfd2a1dbf1ffd0a8c51e306cfd8af4e6
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-26 15:17:27 +0200

    Create bingads_campaign_performance.sql

---
+++ models/reporting/bingads_campaign_performance.sql
@@ -0,0 +1,33 @@
+{{ config (
+    alias = target.database + '_bingads_campaign_performance'
+)}}
+
+WITH office_data as
+    (SELECT office as sf_office, SPLIT_PART(SPLIT_PART(office,' ',1),'R',2) as code, SPLIT_PART(office,' ',2) as location
+    FROM {{ source ('gsheet_raw', 'office_locations') }}
+    GROUP BY office
+    ORDER BY code ASC)
+
+SELECT 
+account_id,
+campaign_name,
+campaign_id,
+campaign_status,
+CASE WHEN campaign_name ~* 'Branded' THEN 'Campaign Type: Branded'
+    WHEN campaign_name ~* 'Metal Roofing Keywords' THEN 'Campaign Type: Non Branded'
+    ELSE 'Campaign Type: Other'
+END as campaign_type_default,
+date,
+date_granularity,
+CASE WHEN location IS NULL THEN 'Unknown' ELSE location END as office,
+sf_office as office_location,
+spend,
+impressions,
+clicks,
+conversions as leads,
+revenue as revenue
+FROM {{ ref('bingads_performance_by_campaign') }}
+LEFT JOIN (SELECT campaign_id, campaign_name, account_id, campaign_status, RIGHT(LEFT(campaign_name,4),3) as code 
+            FROM {{ ref('bingads_campaigns') }}) c 
+    USING(campaign_id, campaign_name, account_id, campaign_status)
+LEFT JOIN office_data USING(code)
