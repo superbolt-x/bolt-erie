1da8d7a8817402af479e54fc9b74e2ff5f4ba62e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-12 14:47:51 -0500

    Update outbrain_campaign_performance.sql

--- models/reporting/outbrain_campaign_performance.sql
+++ models/reporting/outbrain_campaign_performance.sql
@@ -16,7 +16,7 @@ SELECT DATE_TRUNC('day',day::date) as date, 'day' as date_granularity,
   COALESCE(SUM(sum_value),0) as conversions_value
   --leads
 FROM {{ source('outbrain_raw','campaign_report') }}
-LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
 GROUP BY 1,2,3,4,5
 
 UNION ALL
@@ -35,7 +35,7 @@ SELECT DATE_TRUNC('week',day::date) as date, 'week' as date_granularity,
   COALESCE(SUM(sum_value),0) as conversions_value
   --leads
 FROM {{ source('outbrain_raw','campaign_report') }}
-LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
 GROUP BY 1,2,3,4,5
 
 UNION ALL
@@ -54,7 +54,7 @@ SELECT DATE_TRUNC('month',day::date) as date, 'month' as date_granularity,
   COALESCE(SUM(sum_value),0) as conversions_value
   --leads
 FROM {{ source('outbrain_raw','campaign_report') }}
-LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
 GROUP BY 1,2,3,4,5
 
 UNION ALL
@@ -73,7 +73,7 @@ SELECT DATE_TRUNC('quarter',day::date) as date, 'quarter' as date_granularity,
   COALESCE(SUM(sum_value),0) as conversions_value
   --leads
 FROM {{ source('outbrain_raw','campaign_report') }}
-LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
 GROUP BY 1,2,3,4,5
 
 UNION ALL
@@ -92,5 +92,5 @@ SELECT DATE_TRUNC('year',day::date) as date, 'year' as date_granularity,
   COALESCE(SUM(sum_value),0) as conversions_value
   --leads
 FROM {{ source('outbrain_raw','campaign_report') }}
-LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
 GROUP BY 1,2,3,4,5
