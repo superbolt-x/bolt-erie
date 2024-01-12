18ade9acdfd320706bdd4c0cfb5edbd8441b07c2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-12 14:16:44 -0500

    Create outbrain_campaign_performance.sql

---
+++ models/reporting/outbrain_campaign_performance.sql
@@ -0,0 +1,106 @@
+{{ config (
+    alias = target.database + '_outbrain_campaign_performance'
+)}}
+
+SELECT DATE_TRUNC('day',day::date) as date, 'day' as date_granularity,
+  campaign_id,
+  campaign_name,
+  platform,
+  --publisher,
+  --audience,
+  --location,
+  COALESCE(SUM(spend),0) as spend,
+  COALESCE(SUM(clicks),0) as clicks,
+  COALESCE(SUM(impressions),0) as impressions,
+  COALESCE(SUM(conversions),0) as conversions,
+  COALESCE(SUM(sum_value),0) as conversions_value
+  --leads
+FROM {{ source('outbrain_raw','campaign_report') }}
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+/*LEFT JOIN (SELECT * FROM {{ source('outbrain_raw','publisher_report') }} LEFT JOIN {{ source('outbrain_raw','publisher_history') }}
+  USING (campaign_id, */
+GROUP BY 1,2,3,4,5
+
+UNION ALL
+
+SELECT DATE_TRUNC('week',day::date) as date, 'week' as date_granularity,
+  campaign_id,
+  campaign_name,
+  platform,
+  --publisher,
+  --audience,
+  --location,
+  COALESCE(SUM(spend),0) as spend,
+  COALESCE(SUM(clicks),0) as clicks,
+  COALESCE(SUM(impressions),0) as impressions,
+  COALESCE(SUM(conversions),0) as conversions,
+  COALESCE(SUM(sum_value),0) as conversions_value
+  --leads
+FROM {{ source('outbrain_raw','campaign_report') }}
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+/*LEFT JOIN (SELECT * FROM {{ source('outbrain_raw','publisher_report') }} LEFT JOIN {{ source('outbrain_raw','publisher_history') }}
+  USING (campaign_id, */
+GROUP BY 1,2,3,4,5
+
+UNION ALL
+
+SELECT DATE_TRUNC('month',day::date) as date, 'month' as date_granularity,
+  campaign_id,
+  campaign_name,
+  platform,
+  --publisher,
+  --audience,
+  --location,
+  COALESCE(SUM(spend),0) as spend,
+  COALESCE(SUM(clicks),0) as clicks,
+  COALESCE(SUM(impressions),0) as impressions,
+  COALESCE(SUM(conversions),0) as conversions,
+  COALESCE(SUM(sum_value),0) as conversions_value
+  --leads
+FROM {{ source('outbrain_raw','campaign_report') }}
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+/*LEFT JOIN (SELECT * FROM {{ source('outbrain_raw','publisher_report') }} LEFT JOIN {{ source('outbrain_raw','publisher_history') }}
+  USING (campaign_id, */
+GROUP BY 1,2,3,4,5
+
+UNION ALL
+
+SELECT DATE_TRUNC('quarter',day::date) as date, 'quarter' as date_granularity,
+  campaign_id,
+  campaign_name,
+  platform,
+  --publisher,
+  --audience,
+  --location,
+  COALESCE(SUM(spend),0) as spend,
+  COALESCE(SUM(clicks),0) as clicks,
+  COALESCE(SUM(impressions),0) as impressions,
+  COALESCE(SUM(conversions),0) as conversions,
+  COALESCE(SUM(sum_value),0) as conversions_value
+  --leads
+FROM {{ source('outbrain_raw','campaign_report') }}
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+/*LEFT JOIN (SELECT * FROM {{ source('outbrain_raw','publisher_report') }} LEFT JOIN {{ source('outbrain_raw','publisher_history') }}
+  USING (campaign_id, */
+GROUP BY 1,2,3,4,5
+
+UNION ALL
+
+SELECT DATE_TRUNC('year',day::date) as date, 'year' as date_granularity,
+  campaign_id,
+  campaign_name,
+  platform,
+  --publisher,
+  --audience,
+  --location,
+  COALESCE(SUM(spend),0) as spend,
+  COALESCE(SUM(clicks),0) as clicks,
+  COALESCE(SUM(impressions),0) as impressions,
+  COALESCE(SUM(conversions),0) as conversions,
+  COALESCE(SUM(sum_value),0) as conversions_value
+  --leads
+FROM {{ source('outbrain_raw','campaign_report') }}
+LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} USING(campaign_id)
+/*LEFT JOIN (SELECT * FROM {{ source('outbrain_raw','publisher_report') }} LEFT JOIN {{ source('outbrain_raw','publisher_history') }}
+  USING (campaign_id, */
+GROUP BY 1,2,3,4,5
