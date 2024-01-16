4396d7ec8721fde1940b50ff21b219c3f7e6f676
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-16 17:00:13 +0100

    Update outbrain_campaign_performance.sql

--- models/reporting/outbrain_campaign_performance.sql
+++ models/reporting/outbrain_campaign_performance.sql
@@ -5,38 +5,38 @@
 SELECT DATE_TRUNC('day',day::date) as date, 'day' as date_granularity,
   campaign_id,
   campaign_name,
-  platform,
+  --platform,
   --publisher,
   --audience,
   --location,
   COALESCE(SUM(spend),0) as spend,
   COALESCE(SUM(clicks),0) as clicks,
   COALESCE(SUM(impressions),0) as impressions,
-  COALESCE(SUM(conversions),0) as conversions,
-  COALESCE(SUM(sum_value),0) as conversions_value
-  --leads
-FROM {{ source('outbrain_raw','campaign_report') }}
+  COALESCE(SUM(total_conversions),0) as conversions,
+  COALESCE(SUM(total_conversion_value),0) as conversions_value,
+  COALESCE(SUM(total_conversion_value),0) as leads
+FROM {{ source('gsheet_raw','outbrain_campaign_insights') }}
 LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
 
 UNION ALL
 
 SELECT DATE_TRUNC('week',day::date) as date, 'week' as date_granularity,
   campaign_id,
   campaign_name,
-  platform,
+  --platform,
   --publisher,
   --audience,
   --location,
   COALESCE(SUM(spend),0) as spend,
   COALESCE(SUM(clicks),0) as clicks,
   COALESCE(SUM(impressions),0) as impressions,
-  COALESCE(SUM(conversions),0) as conversions,
-  COALESCE(SUM(sum_value),0) as conversions_value
-  --leads
-FROM {{ source('outbrain_raw','campaign_report') }}
+  COALESCE(SUM(total_conversions),0) as conversions,
+  COALESCE(SUM(total_conversion_value),0) as conversions_value,
+  COALESCE(SUM(total_conversion_value),0) as leads
+FROM {{ source('gsheet_raw','outbrain_campaign_insights') }}
 LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
 
 UNION ALL
 
@@ -50,12 +50,12 @@ SELECT DATE_TRUNC('month',day::date) as date, 'month' as date_granularity,
   COALESCE(SUM(spend),0) as spend,
   COALESCE(SUM(clicks),0) as clicks,
   COALESCE(SUM(impressions),0) as impressions,
-  COALESCE(SUM(conversions),0) as conversions,
-  COALESCE(SUM(sum_value),0) as conversions_value
-  --leads
-FROM {{ source('outbrain_raw','campaign_report') }}
+  COALESCE(SUM(total_conversions),0) as conversions,
+  COALESCE(SUM(total_conversion_value),0) as conversions_value,
+  COALESCE(SUM(total_conversion_value),0) as leads
+FROM {{ source('gsheet_raw','outbrain_campaign_insights') }}
 LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
 
 UNION ALL
 
@@ -69,28 +69,28 @@ SELECT DATE_TRUNC('quarter',day::date) as date, 'quarter' as date_granularity,
   COALESCE(SUM(spend),0) as spend,
   COALESCE(SUM(clicks),0) as clicks,
   COALESCE(SUM(impressions),0) as impressions,
-  COALESCE(SUM(conversions),0) as conversions,
-  COALESCE(SUM(sum_value),0) as conversions_value
-  --leads
-FROM {{ source('outbrain_raw','campaign_report') }}
+  COALESCE(SUM(total_conversions),0) as conversions,
+  COALESCE(SUM(total_conversion_value),0) as conversions_value,
+  COALESCE(SUM(total_conversion_value),0) as leads
+FROM {{ source('gsheet_raw','outbrain_campaign_insights') }}
 LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
 
 UNION ALL
 
 SELECT DATE_TRUNC('year',day::date) as date, 'year' as date_granularity,
   campaign_id,
   campaign_name,
-  platform,
+  --platform,
   --publisher,
   --audience,
   --location,
   COALESCE(SUM(spend),0) as spend,
   COALESCE(SUM(clicks),0) as clicks,
   COALESCE(SUM(impressions),0) as impressions,
-  COALESCE(SUM(conversions),0) as conversions,
-  COALESCE(SUM(sum_value),0) as conversions_value
-  --leads
-FROM {{ source('outbrain_raw','campaign_report') }}
+  COALESCE(SUM(total_conversions),0) as conversions,
+  COALESCE(SUM(total_conversion_value),0) as conversions_value,
+  COALESCE(SUM(total_conversion_value),0) as leads
+FROM {{ source('gsheet_raw','outbrain_campaign_insights') }}
 LEFT JOIN (SELECT id as campaign_id, name as campaign_name FROM {{ source('outbrain_raw','campaign_history') }} ) USING(campaign_id)
-GROUP BY 1,2,3,4,5
+GROUP BY 1,2,3,4
