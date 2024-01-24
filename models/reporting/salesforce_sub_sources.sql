f748a63fee9c02a8c18265ca2b8760b7d063993f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-24 11:38:47 -0500

    Update salesforce_sub_sources.sql
    utm additions

--- models/reporting/salesforce_sub_sources.sql
+++ models/reporting/salesforce_sub_sources.sql
@@ -2,9 +2,9 @@
     alias = target.database + '_salesforce_sub_sources'
 )}}
 
-SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR') THEN 'Facebook'
-            WHEN source IN ('SM2','SM4','RYT','BRYT','BSM2','BSM4') THEN 'YouTube'
-            WHEN source IN ('PMX','BPMX','IL2','SMD','BIL2','BSMD') THEN 'Google'
+SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR') OR utm_source = 'facebook' THEN 'Facebook'
+            WHEN source IN ('SM2','SM4','RYT','BRYT','BSM2','BSM4') OR utm_source = 'youtube' THEN 'YouTube'
+            WHEN source IN ('PMX','BPMX','IL2','SMD','BIL2','BSMD') OR utm_source = 'google' THEN 'Google'
             WHEN source = 'SM6' THEN 'TikTok'
             WHEN source IN ('IL3','BIL3') THEN 'Bing'
             WHEN source IN ('SM5','BSM5') THEN 'Nextdoor'
@@ -16,6 +16,7 @@ SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR') THEN 'Fa
         office, 
         office_location, 
         CASE WHEN market IS NULL THEN '999 - Invalid' ELSE market END as sf_locations, 
+        source,
         sub_source_id, 
         sub_source, 
         zip,
@@ -27,16 +28,26 @@ SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR') THEN 'Fa
             WHEN source = 'RYT' THEN 'Retargeting'
             ELSE 'Other'
         END as market,
-        CASE WHEN source IN ('SM2','SM4','SM1','SM','BSM','BSM2') THEN 'Prospecting'
-            WHEN source IN ('SMR','SMO','BSMR') THEN 'Retargeting'
-            WHEN source IN ('SMD','BSMD') THEN 'Discovery'
-            WHEN source IN ('PMX','BPMX') THEN 'Performance Max'
-            WHEN source IN ('IL2','BIL2','IL3','BIL3') THEN 'Search'
+        CASE WHEN source IN ('SM2','SM4','SM1','SM','BSM','BSM2') 
+                OR (utm_source = 'facebook' AND utm_campaign !~* 'warm') THEN 'Prospecting'
+            WHEN source IN ('SMR','SMO','BSMR')
+                OR (utm_source = 'facebook' AND utm_campaign ~* 'warm') THEN 'Retargeting'
+            WHEN source IN ('SMD','BSMD') OR (utm_source = 'google' AND advertising_channel_type = 'DISCOVERY') THEN 'Discovery'
+            WHEN source IN ('PMX','BPMX') OR (utm_source = 'google' AND advertising_channel_type = 'PERFORMANCE_MAX') THEN 'Performance Max'
+            WHEN source IN ('IL2','BIL2','IL3','BIL3') OR (utm_source = 'google' AND advertising_channel_type = 'SEARCH')THEN 'Search'
         END as campaign_type,
         NULL as campaign_name,
         dispo,
         call_disposition,
         status_detail,
+        utm_medium,
+        utm_campaign,
+        utm_term,
+        utm_content,
+        utm_keyword,
+        utm_match_type,
+        utm_placement,
+        utm_discount,
         0 AS spend,
         0 AS clicks,
         0 AS impressions,
@@ -55,5 +66,7 @@ SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR') THEN 'Fa
         COALESCE(SUM(issues),0) as issues,
         COALESCE(SUM(ooa_leads),0) as ooa_leads
     FROM {{ source('reporting','salesforce_performance') }}
+    LEFT JOIN (SELECT campaign_id as utm_campaign, campaign_name, advertising_channel_type
+            FROM {{ ref('googleads_campaigns') }}) USING(utm_campaign)
     WHERE date >= '2022-12-01'
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
