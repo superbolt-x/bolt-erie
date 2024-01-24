818a6ec4e2b9b2a9a7e00e2db2fd3c27a4988bd8
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-24 12:00:25 -0500

    Update googleads_sub_sources_google.sql
    utm additions

--- models/reporting/googleads_sub_sources_google.sql
+++ models/reporting/googleads_sub_sources_google.sql
@@ -220,6 +220,7 @@ SELECT
         office, 
         office_location, 
         NULL as sf_locations, 
+        NULL as source, 
         sub_source_id, 
         sub_source,
         NULL as zip, 
@@ -234,10 +235,17 @@ SELECT
                 OR advertising_channel_type = 'SEARCH')
             THEN 'Search'
         END as campaign_type,
-        campaign_name,
         NULL as dispo,
         NULL as call_disposition,
         NULL as status_detail,
+        NULL as utm_medium,
+        campaign_id as utm_campaign,
+        NULL as utm_term,
+        NULL as utm_content,
+        NULL as utm_keyword,
+        NULL as utm_match_type,
+        NULL as utm_placement,
+        NULL as utm_discount,
         COALESCE(SUM(spend),0) AS spend,
         COALESCE(SUM(clicks),0) AS clicks,
         COALESCE(SUM(impressions),0) AS impressions,
@@ -257,4 +265,4 @@ SELECT
         0 as ooa_leads
     FROM (SELECT * FROM final_data)
     WHERE date >= '2022-12-01'
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
