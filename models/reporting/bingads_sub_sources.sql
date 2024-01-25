f8fe5c3b9e5d3000244ff03ab7d933f2783b57d4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-01-25 13:05:30 -0500

    Update bingads_sub_sources.sql
    ad breakdown

--- models/reporting/bingads_sub_sources.sql
+++ models/reporting/bingads_sub_sources.sql
@@ -17,13 +17,15 @@ SELECT ad_group_id,
         office, 
         office_location, 
         campaign_name,
+        campaign_id, 
+        ad_id,
         ad_group_id,
         COALESCE(SUM(spend),0) AS spend,
         COALESCE(SUM(clicks),0) AS clicks,
         COALESCE(SUM(impressions),0) AS impressions,
         COALESCE(SUM(leads),0) AS inplatform_leads
     FROM {{ source('reporting','bingads_ad_performance') }}
-    GROUP BY 1,2,3,4,5,6)
+    GROUP BY 1,2,3,4,5,6,7,8,9)
 
 , joined_data as 
     (SELECT 
@@ -32,13 +34,16 @@ SELECT ad_group_id,
         office, 
         office_location, 
         campaign_name,
+        campaign_id, 
+        ad_id,
+        ad_group_id,
         sub_source_id,
         COALESCE(SUM(spend),0) AS spend,
         COALESCE(SUM(clicks),0) AS clicks,
         COALESCE(SUM(impressions),0) AS impressions,
         COALESCE(SUM(inplatform_leads),0) AS inplatform_leads
     FROM bingads_data left join sub_source_data USING(ad_group_id)
-    GROUP BY 1,2,3,4,5,6)
+    GROUP BY 1,2,3,4,5,6,7,8,9)
 
 , sf_data as 
     (SELECT date, date_granularity, sub_source_id, sub_source,
@@ -79,9 +84,9 @@ SELECT
         NULL as call_disposition,
         NULL as status_detail,
         NULL as utm_medium,
-        campaign_name as utm_campaign,
-        NULL as utm_term,
-        NULL as utm_content,
+        campaign_id::VARCHAR as utm_campaign,
+        ad_group_id::VARCHAR as utm_term,
+        ad_id::VARCHAR as utm_content,
         NULL as utm_keyword,
         NULL as utm_match_type,
         NULL as utm_placement,
