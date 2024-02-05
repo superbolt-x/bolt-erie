ed3db4b421bc5b58eafabf101ed76f664a015226
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-05 10:27:33 -0500

    Update salesforce_performance.sql
    replace youtube ads with dash as space

--- models/reporting/salesforce_performance.sql
+++ models/reporting/salesforce_performance.sql
@@ -44,7 +44,7 @@ WITH office_data as
     SELECT DATE_TRUNC('week',lead_entry_date::date+1)-1 as date, 'week' as date_granularity,
         market, state, source, zip,sub_source_id, sub_source, dispo, call_disposition, status_detail, 
         utm_source, utm_medium, utm_campaign, utm_term, 
-        CASE WHEN source IN ('SM2','SM4','RYT','BRYT','BSM2','BSM4') OR utm_source = 'youtube' THEN REPLACE(utm_content,'_',' ') ELSE utm_content END as utm_content_adj,
+        CASE WHEN source IN ('SM2','SM4','RYT','BRYT','BSM2','BSM4') OR utm_source = 'youtube' THEN REPLACE(REPLACE(utm_content,'_',' '),'-','') ELSE utm_content END as utm_content_adj,
         utm_keyword, utm_match_type, utm_placement, utm_discount,
         COUNT(DISTINCT lead_id) as leads,
         COALESCE(SUM(number_of_calls),0) as calls,
