effb0c4cd4e0455bb4adbe0e25a56419116928ea
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-14 17:01:06 +0100

    Update googleads_sub_sources_google.sql

--- models/reporting/googleads_sub_sources_google.sql
+++ models/reporting/googleads_sub_sources_google.sql
@@ -12,7 +12,7 @@ WITH subsource_cte as (
  subsource_id_cte as (
         
         select  
-                ad_final_urls,{{ get_date_parts('date') }},
+                ad_final_urls,
                 case
                     when RIGHT(ad_final_urls, 5) = 'tep/]' then LEFT(RIGHT(ad_final_urls, 16),3)
                         when (ad_final_urls = '[http://go.eriemetalroofs.com/erie-youtube-metal-roofing-f/]' 
@@ -35,7 +35,7 @@ WITH subsource_cte as (
                 end as sub_source_id,
                 sum(cost_micros::FLOAT)
         from {{ source('googleads_raw','ad_performance_report') }}
-        group by 1,2,3,4,5,6,7),
+        group by 1,2),
 
 campaign_max_updated_date as (
  SELECT id , max(updated_at) as max_updated_at
@@ -85,7 +85,7 @@ joined_data as  ( (
                     {{date_granularity}} as date,
                     advertising_channel_type,
                     sum(cost_micros::FLOAT/1000000::FLOAT) as spends
-                    from {{ source('googleads_raw', 'ad_performance_report') }}
+                    from (select *, {{ get_date_parts('date') }} from {{ source('googleads_raw', 'ad_performance_report') }})
                     left join campaign_types
                     USING(campaign_id)
                     left join subsource_id_cte
