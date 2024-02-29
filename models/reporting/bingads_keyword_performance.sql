c48f8881d17d5657e30febe06dbc98019e5d6c3e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-02-29 15:43:11 +0100

    Create bingads_keyword_performance.sql

---
+++ models/reporting/bingads_keyword_performance.sql
@@ -0,0 +1,41 @@
+{{ config (
+    alias = target.database + '_bingads_keyword_performance'
+)}}
+
+WITH office_data as
+    (SELECT office as sf_office, SPLIT_PART(SPLIT_PART(office,' ',1),'R',2) as code, SPLIT_PART(office,' ',2) as location
+    FROM {{ source('gsheet_raw', 'office_locations') }}
+    GROUP BY office
+    ORDER BY code ASC)
+
+SELECT 
+account_id,
+ad_id,
+keyword_id,
+name as keyword_name,
+case 
+  when match_type = 'Broad' then 'b'
+  when match_type = 'Phrase' then 'p'
+  when match_type = 'Exact' then 'e'
+end as keyword_match_type,
+ad_group_name,
+ad_status,
+ad_group_id,
+ad_group_status,
+c.campaign_name,
+campaign_id,
+c.campaign_status,
+date,
+date_granularity,
+CASE WHEN location IS NULL THEN 'Unknown' ELSE location END as office,
+sf_office as office_location,
+spend,
+impressions,
+clicks,
+conversions as leads,
+revenue as revenue
+FROM {{ ref('bingads_performance_by_keyword') }}
+LEFT JOIN (SELECT campaign_id, campaign_name, account_id, campaign_status, RIGHT(LEFT(campaign_name,4),3) as code 
+          FROM {{ ref('bingads_campaigns') }}) c 
+    USING(campaign_id, account_id)
+LEFT JOIN office_data USING(code)
