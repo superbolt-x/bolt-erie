6cadcd719bd94c4a0e964a41ef74ea1293061d10
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-21 12:13:13 -0400

    Create nextdoor_ad_performance.sql

---
+++ models/reporting/nextdoor_ad_performance.sql
@@ -0,0 +1,46 @@
+{{ config (
+    alias = target.database + '_nextdoor_ad_performance'
+)}}
+    
+{% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
+
+WITH initial_data as
+  (SELECT *, {{ get_date_parts('start_time') }} 
+  FROM {{ source('s3_raw','nextdoor_daily_performance') }} 
+  WHERE _modified IN (SELECT MAX(_modified) FROM {{ source('s3_raw','nextdoor_daily_performance') }} )),
+
+  final_data as
+  {%- for date_granularity in date_granularity_list %}    
+  (SELECT 
+      '{{date_granularity}}' as date_granularity,
+      {{date_granularity}} as date,
+      ad_id,
+      ad_name,
+      ad_group_id,
+      ad_group_name,
+      campaign_id,
+      campaign_name,
+      COALESCE(SUM(spend),0) as spend,
+      COALESCE(SUM(clicks),0) as clicks,
+      COALESCE(SUM(impressions),0) as impressions,
+      COALESCE(SUM(conversions),0) as conversions
+  FROM initial_data
+  GROUP BY 1,2,3,4,5,6,7,8
+  {% if not loop.last %}UNION ALL
+  {% endif %}
+{% endfor %})
+
+SELECT 
+    date,
+    date_granularity,
+    ad_id,
+    ad_name,
+    ad_group_id,
+    ad_group_name,
+    campaign_id,
+    campaign_name,
+    spend,
+    impressions,
+    clicks,
+    conversions
+FROM final_data
