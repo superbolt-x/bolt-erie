753488d632d021936ec90236eea30ab3978e8700
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-08-26 15:56:34 -0600

    Update googleads_campaign_hourly_performance.sql
    changed conversion_action_name ~* '(Kashurba) Get Pricing' to conversion_action_name = '(Kashurba) Get Pricing'

--- models/reporting/googleads_campaign_hourly_performance.sql
+++ models/reporting/googleads_campaign_hourly_performance.sql
@@ -3,26 +3,20 @@
 )}}
 
 WITH initial_data as
-(SELECT date, hour, campaign_name, campaign_id, 'Basement' as erie_type, _fivetran_synced,
-    COALESCE(SUM(cost),0) as spend, 0 as leads,
-    MAX(_fivetran_synced) OVER (PARTITION by date, hour, campaign_name, campaign_id) as last_updated_date
+(SELECT date, hour, campaign_name, campaign_id, 'Basement' as erie_type, 
+    COALESCE(SUM(cost),0) as spend, 0 as leads
   FROM {{ source('gsheet_raw','campaign_hourly_basement_report') }}
-  GROUP BY 1,2,3,4,5,_fivetran_synced
+  GROUP BY 1,2,3,4,5
   UNION ALL
-  SELECT date, hour, campaign_name, campaign_id, 'Roofing' as erie_type, _fivetran_synced,
-    COALESCE(SUM(cost),0) as spend, 0 as leads,
-    MAX(_fivetran_synced) OVER (PARTITION by date, hour, campaign_name, campaign_id) as last_updated_date
+  SELECT date, hour, campaign_name, campaign_id, 'Roofing' as erie_type, 
+    COALESCE(SUM(cost),0) as spend, 0 as leads
   FROM {{ source('gsheet_raw','campaign_hourly_roof_report') }}
-  GROUP BY 1,2,3,4,5,_fivetran_synced
+  GROUP BY 1,2,3,4,5
   UNION ALL
-  SELECT date, hour, campaign_name, campaign_id, CASE WHEN customer_id = 4560674777 THEN 'Roofing' WHEN customer_id = 2819798401 THEN 'Basement' END as erie_type, _fivetran_synced,
-    0 as spend, COALESCE(SUM(CASE WHEN conversion_action_name ~* '(Kashurba) Get Pricing' THEN all_conversions END),0) as leads,
-    MAX(_fivetran_synced) OVER (PARTITION by date, hour, campaign_name, campaign_id) as last_updated_date
+  SELECT date, hour, campaign_name, campaign_id, CASE WHEN customer_id = 4560674777 THEN 'Roofing' WHEN customer_id = 2819798401 THEN 'Basement' END as erie_type, 
+    0 as spend, COALESCE(SUM(CASE WHEN conversion_action_name = '(Kashurba) Get Pricing' THEN all_conversions END),0) as leads
   FROM {{ source('gsheet_raw','campaign_hourly_convtype_report') }}
-  GROUP BY 1,2,3,4,5,_fivetran_synced),
-  
-  final_data as 
-  (SELECT * FROM initial_data WHERE _fivetran_synced = last_updated_date)
+  GROUP BY 1,2,3,4,5)
 
   SELECT 
     date,
@@ -32,5 +26,5 @@ WITH initial_data as
     erie_type,
     COALESCE(SUM(spend),0) as spend,
     COALESCE(SUM(leads),0) as leads
-  FROM final_data
+  FROM initial_data
   GROUP BY 1,2,3,4,5
