4cbdeb1e37fd93ba905827d9a2bc4fd2cdf14c8f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-20 21:04:35 +0100

    Update tiktok_sub_sources.sql

--- models/reporting/tiktok_sub_sources.sql
+++ models/reporting/tiktok_sub_sources.sql
@@ -94,7 +94,9 @@ SELECT 'TikTok' AS channel,
         0 as workable_leads,
         0 as hits,
         0 as issues,
-        0 as ooa_leads
+        0 as ooa_leads,
+        0 AS inplatform_workable_leads,
+        0 AS inplatform_appointments
     FROM {{ source('reporting','tiktok_ad_performance') }}
     LEFT JOIN source using(ad_id, date, date_granularity)
     WHERE date >= '2022-12-01'
