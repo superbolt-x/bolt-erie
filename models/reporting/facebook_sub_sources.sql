95bd3b1251ef9f0b86775988a24ba3cf5f760ba8
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-20 21:04:44 +0100

    Update facebook_sub_sources.sql

--- models/reporting/facebook_sub_sources.sql
+++ models/reporting/facebook_sub_sources.sql
@@ -62,7 +62,9 @@ SELECT
         0 as workable_leads,
         0 as hits,
         0 as issues,
-        0 as ooa_leads
+        0 as ooa_leads,
+        0 AS inplatform_workable_leads,
+        0 AS inplatform_appointments
     FROM {{ source('reporting','facebook_ad_performance') }}
     WHERE date >= '2022-12-01'
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
