ac7dd2fb976b13a15f65104eec4905f827842077
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-14 09:59:00 +0100

    Update facebook_campaign_performance.sql

--- models/reporting/facebook_campaign_performance.sql
+++ models/reporting/facebook_campaign_performance.sql
@@ -36,7 +36,7 @@ link_clicks,
 website_leads+onfacebook_leads as leads
 FROM 
    {{ ref('facebook_performance_by_campaign') }} 
-LEFT JOIN (SELECT campaign_id::VARCHAR as campaign_id, campaign_name, account_id::INT as account_id, campaign_effective_status, 
+LEFT JOIN (SELECT campaign_id::VARCHAR as campaign_id, campaign_name, campaign_effective_status, 
         case
             when (account_id = '813620678687014' OR account_id = '306770030564777') AND campaign_name !~* 'soc -' then RIGHT(LEFT(campaign_name,4),3) 
             when account_id = '1349056908916556' AND LEFT(campaign_name,4) = 'B071' AND campaign_name !~* 'soc -' THEN 'B001'
@@ -48,5 +48,5 @@ LEFT JOIN (SELECT campaign_id::VARCHAR as campaign_id, campaign_name, account_id
             when account_id = '1349056908916556' AND campaign_name ~* 'soc -' then RIGHT(trim(split_part(campaign_name,'-',6)),4)
         end as code 
         FROM {{ ref('facebook_campaigns') }}) c
-        USING(campaign_id, campaign_name, account_id, campaign_effective_status)
+        USING(campaign_id, campaign_name, campaign_effective_status)
     LEFT JOIN office_data USING(code)
