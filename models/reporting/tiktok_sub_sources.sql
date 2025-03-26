e3996977f99d3702d13e227fefbeedc6b5bf451f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-26 16:38:45 +0100

    Update tiktok_sub_sources.sql

--- models/reporting/tiktok_sub_sources.sql
+++ models/reporting/tiktok_sub_sources.sql
@@ -78,6 +78,7 @@ SELECT 'TikTok' AS channel,
         NULL as utm_match_type,
         NULL as utm_placement,
         NULL as utm_discount,
+        NULL as utm_lp_variant,
         COALESCE(SUM(spends),0) AS spend,
         COALESCE(SUM(clicks),0) AS clicks,
         COALESCE(SUM(impressions),0) AS impressions,
@@ -100,6 +101,6 @@ SELECT 'TikTok' AS channel,
     FROM {{ source('reporting','tiktok_ad_performance') }}
     LEFT JOIN source using(ad_id, date, date_granularity)
     WHERE date >= '2022-12-01'
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26)
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27)
     
 select * from joined_data
