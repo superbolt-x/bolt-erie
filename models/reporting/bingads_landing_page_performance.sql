f5ab65e58311f38cee947f99fe44aeaf234c879b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-18 15:38:40 -0600

    Update bingads_landing_page_performance.sql

--- models/reporting/bingads_landing_page_performance.sql
+++ models/reporting/bingads_landing_page_performance.sql
@@ -34,7 +34,10 @@ final_data as
         COALESCE(SUM(impressions),0) as impressions,
         COALESCE(SUM(clicks),0) as clicks 
     FROM lp_data
-    GROUP BY 1,2,3,4,5,6,7,8,9)
+    GROUP BY 1,2,3,4,5,6,7,8,9
+        {% if not loop.last %}UNION ALL
+        {% endif %}
+    {% endfor %})
 
 SELECT 
 account_id,
