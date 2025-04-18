2d38549c324212ffbb3843966142beb541be8cdd
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-18 14:52:56 -0600

    Create bingads_landing_page_performance.sql

---
+++ models/reporting/bingads_landing_page_performance.sql
@@ -0,0 +1,59 @@
+{{ config (
+    alias = target.database + '_bingads_landing_page_performance'
+)}}
+
+WITH office_data as
+    (SELECT COUNT(*),
+        CASE WHEN office ~* 'R062' THEN 'R062 West Atlanta-GA' ELSE office END as office_adj,
+        office_adj as sf_office,
+        SPLIT_PART(SPLIT_PART(office_adj,' ',1),'R',2) as code, 
+        SPLIT_PART(office_adj,' ',2) as location
+    FROM {{ source('gsheet_raw', 'office_locations') }}
+    GROUP BY 2,3,4,5
+    ORDER BY code ASC),
+
+lp_data as 
+    (SELECT account_id::varchar as account_id, campaign_id, final_url as landing_page,
+      impressions, clicks, spend, 
+      {{ get_date_parts('date') }}
+    FROM {{ source('bingads_raw', 'destination_url_performance_daily_report') }})
+  
+final_data as
+    ({%- for date_granularity in date_granularity_list %}
+    SELECT 
+        '{{date_granularity}}' as date_granularity,
+        {{date_granularity}} as date,
+        account_id,
+        campaign_id,
+        landing_page,
+        COALESCE(SUM(spend),0) as spend,
+        COALESCE(SUM(impressions),0) as impressions,
+        COALESCE(SUM(clicks),0) as clicks 
+    FROM lp_data
+    GROUP BY 1,2,3,4,5)
+
+SELECT 
+account_id,
+campaign_id,
+c.campaign_name,
+CASE WHEN c.campaign_name ~* 'Branded' THEN 'Campaign Type: Branded'
+    WHEN c.campaign_name ~* 'Metal Roofing Keywords' THEN 'Campaign Type: Non Branded'
+    ELSE 'Campaign Type: Other'
+END as campaign_type_default,
+landing_page,
+date,
+date_granularity,
+CASE WHEN account_id = '149506166' THEN 'Basement'
+    WHEN account_id = '149034657' THEN 'Roofing'
+    END AS erie_type,
+'National' as market,
+CASE WHEN location IS NULL THEN 'Unknown' ELSE location END as office, 
+sf_office as office_location, 
+spend,
+impressions,
+clicks  
+FROM final_data
+LEFT JOIN (SELECT campaign_id, campaign_name, account_id, RIGHT(LEFT(campaign_name,4),3) as code 
+            FROM {{ ref('bingads_campaigns') }}) c 
+    USING(campaign_id, account_id)
+LEFT JOIN office_data USING(code)
