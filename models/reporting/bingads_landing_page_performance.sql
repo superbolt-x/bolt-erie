dc532269fdf762873a93562a510c9ee5e0eb0b8b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-08-18 10:43:00 -0600

    Update bingads_landing_page_performance.sql
    lp_data_four

--- models/reporting/bingads_landing_page_performance.sql
+++ models/reporting/bingads_landing_page_performance.sql
@@ -30,7 +30,7 @@ lp_data as
     SELECT date, account_id::varchar as account_id, ad_id, ad_group_id, ad_group_name, campaign_id, campaign_name, final_url as landing_page,
         CASE WHEN landing_page ~* 'https://get.eriehome.com/affordable-metal-roofing/' THEN 'affordable-metal-roofing_l'
             WHEN landing_page ~* 'nations-number-one-roofing-contractor' THEN 'nations-number-one-roofing-contractor_r'
-            WHEN landing_page ~* 'we-need-old-roofs' THEN 'we-need-old-roofs_f'
+            WHEN landing_page ~* 'we-need-old-roofs' THEN 'we-need-old-roofs_g'
             ELSE 'Other'
         END as lp_variant,
         COALESCE(SUM(impressions::float/2::float),0) AS impressions, 
@@ -69,6 +69,27 @@ lp_data_three as
         COALESCE(SUM(spend*0.33),0) AS spend
     FROM {{ source('bingads_raw', 'destination_url_performance_daily_report') }} 
     GROUP BY 1,2,3,4,5,6,7,8,9),
+
+lp_data_four as
+    (SELECT date, account_id::varchar as account_id, ad_id, ad_group_id, ad_group_name, campaign_id, campaign_name, final_url as landing_page,
+        CASE WHEN landing_page ~* 'https://get.eriehome.com/homes-with-old-roofs-wanted/' THEN 'homes-with-old-roofs-wanted_e'
+            ELSE 'Other'
+        END as lp_variant,
+        COALESCE(SUM(impressions*0.75),0) AS impressions, 
+        COALESCE(SUM(clicks*0.75),0) AS clicks, 
+        COALESCE(SUM(spend*0.75),0) AS spend
+    FROM {{ source('bingads_raw', 'destination_url_performance_daily_report') }} 
+    GROUP BY 1,2,3,4,5,6,7,8,9
+    UNION ALL
+    SELECT date, account_id::varchar as account_id, ad_id, ad_group_id, ad_group_name, campaign_id, campaign_name, final_url as landing_page,
+        CASE WHEN landing_page ~* 'https://get.eriehome.com/homes-with-old-roofs-wanted/' THEN 'homes-with-old-roofs-wanted_f'
+            ELSE 'Other'
+        END as lp_variant,
+        COALESCE(SUM(impressions*0.25),0) AS impressions, 
+        COALESCE(SUM(clicks*0.25),0) AS clicks, 
+        COALESCE(SUM(spend*0.25),0) AS spend
+    FROM {{ source('bingads_raw', 'destination_url_performance_daily_report') }} 
+    GROUP BY 1,2,3,4,5,6,7,8,9),
     
 initial_data as 
     (SELECT account_id, ad_group_id, ad_group_name, campaign_id, campaign_name, landing_page, lp_variant, impressions, clicks, spend, 
@@ -76,7 +97,9 @@ initial_data as
     FROM 
         (SELECT * FROM lp_data
         UNION ALL
-        SELECT * FROM lp_data_three)
+        SELECT * FROM lp_data_three
+        UNION ALL
+        SELECT * FROM lp_data_four)
     ),
   
 final_data as
