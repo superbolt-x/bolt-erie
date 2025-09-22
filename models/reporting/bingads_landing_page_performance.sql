568c672fcbb5c9c6e381c04841413b54b3927fac
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-09-22 20:11:46 +0200

    Update bingads_landing_page_performance.sql

--- models/reporting/bingads_landing_page_performance.sql
+++ models/reporting/bingads_landing_page_performance.sql
@@ -16,7 +16,7 @@ WITH office_data as
 
 lp_data as
     (SELECT date, account_id::varchar as account_id, ad_id, ad_group_id, ad_group_name, campaign_id, campaign_name, final_url as landing_page,
-        CASE WHEN landing_page ~* 'https://get.eriehome.com/affordable-metal-roofing/' THEN 'affordable-metal-roofing_m'
+        CASE WHEN landing_page ~* 'https://get.eriehome.com/affordable-metal-roofing/' THEN 'affordable-metal-roofing_o'
             WHEN landing_page ~* 'nations-number-one-roofing-contractor' THEN 'nations-number-one-roofing-contractor_n'
             WHEN landing_page ~* 'we-need-old-roofs' THEN 'we-need-old-roofs_a'
             ELSE 'Other'
