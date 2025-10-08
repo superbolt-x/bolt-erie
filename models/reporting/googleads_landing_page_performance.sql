e876ab25a44a64c8dc3d6408092affad48ef271b
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-10-08 17:33:31 +0200

    Update googleads_landing_page_performance.sql

--- models/reporting/googleads_landing_page_performance.sql
+++ models/reporting/googleads_landing_page_performance.sql
@@ -35,7 +35,7 @@ lp_data as
     SELECT date, customer_id as account_id, campaign_id, ad_group_id, unexpanded_final_url as landing_page,
         CASE WHEN landing_page ~* 'https://get.eriehome.com/affordable-metal-roofing' THEN 'affordable-metal-roofing_l'
             WHEN landing_page ~* 'nations-number-one-roofing-contractor' THEN 'nations-number-one-roofing-contractor_r'
-            WHEN landing_page ~* 'we-need-old-roofs' THEN 'we-need-old-roofs_g'
+            WHEN landing_page ~* 'we-need-old-roofs' THEN 'we-need-old-roofs_h'
             WHEN landing_page ~* 'https://get.eriehome.com/homes-with-old-roofs-wanted' THEN 'homes-with-old-roofs-wanted_g'
             WHEN landing_page ~* 'https://get.eriehome.com/nations-number-one-roofing' THEN 'nations-number-one-roofing_n'
             ELSE 'Other'
