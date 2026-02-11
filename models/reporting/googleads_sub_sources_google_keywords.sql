844907ff2b7ca1e84407504240b32bfda6fa30b4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-02-11 10:57:23 +0100

    Update googleads_sub_sources_google_keywords.sql

--- models/reporting/googleads_sub_sources_google_keywords.sql
+++ models/reporting/googleads_sub_sources_google_keywords.sql
@@ -154,7 +154,8 @@ SELECT
         0 as issues,
         0 as ooa_leads,
         COALESCE(SUM(workable_leads),0) AS inplatform_workable_leads,
-        COALESCE(SUM(appointments),0) AS inplatform_appointments
+        COALESCE(SUM(appointments),0) AS inplatform_appointments,
+        0 as set_value
     FROM (SELECT * FROM final_data)
     WHERE date >= '2022-12-01'
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
