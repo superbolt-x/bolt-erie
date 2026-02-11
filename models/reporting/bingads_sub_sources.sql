b04cf3ae935cb5d30267110e7461295cd098cd26
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-02-11 10:56:36 +0100

    Add set_value column to SQL query

--- models/reporting/bingads_sub_sources.sql
+++ models/reporting/bingads_sub_sources.sql
@@ -157,7 +157,8 @@ SELECT
         0 as ooa_leads,
         0 as net_sale_count,
         0 AS inplatform_workable_leads,
-        0 AS inplatform_appointments
+        0 AS inplatform_appointments,
+        0 as set_value
     FROM joined_data LEFT JOIN sf_data USING(date,date_granularity,sub_source_id)
     WHERE date >= '2022-12-01'
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
