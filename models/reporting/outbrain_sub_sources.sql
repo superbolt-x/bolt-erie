296f0a1869f5231156955e9e101cca859c1101b4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-02-11 10:57:28 +0100

    Update outbrain_sub_sources.sql

--- models/reporting/outbrain_sub_sources.sql
+++ models/reporting/outbrain_sub_sources.sql
@@ -48,6 +48,7 @@ SELECT 'Outbrain' AS channel,
         0 as ooa_leads,
         0 as net_sale_count,
         0 AS inplatform_workable_leads,
-        0 AS inplatform_appointments
+        0 AS inplatform_appointments,
+        0 as set_value
     FROM {{ source('reporting','outbrain_campaign_performance') }}
     GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
