4e811b619396f73ee96f83fe9bc3f38a654e49f0
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-05-29 16:26:36 +0200

    Update CASE logic in salesforce_sub_sources.sql
    Refactor CASE statements for utm_campaign and utm_term to improve clarity and logic.

--- models/reporting/salesforce_sub_sources.sql
+++ models/reporting/salesforce_sub_sources.sql
@@ -63,11 +63,11 @@ SELECT CASE WHEN source IN ('SM','SMR','SMO','SM1','SM13','BSM','BSMR','BSM1') O
             ELSE utm_campaign::VARCHAR
         END as utm_campaign,
         CASE WHEN source IN ('PMX','BPMX','IL2','SMD','BIL2','BSMD') OR utm_source = 'google' THEN gb_ad_group_name::VARCHAR
-            WHEN source IN ('IL3','BIL3','BNA','PMX2') OR utm_source = 'bing' THEN gb_ad_group_name::VARCHAR
+            WHEN source IN ('IL3','BIL3') OR (utm_source = 'bing' AND source NOT IN ('BNA','PMX2')) THEN gb_ad_group_name::VARCHAR
             ELSE utm_term::VARCHAR
         END as utm_term,
         utm_content::VARCHAR,
-        utm_keyword,
+        CASE WHEN (utm_source = 'bing' AND source NOT IN ('BNA','PMX2')) THEN NULL ELSE utm_keyword AS utm_keyword,
         '' as utm_match_type,
         utm_placement,
         utm_discount,
