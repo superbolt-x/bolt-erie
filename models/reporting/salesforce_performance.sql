d6fc807ef37188b34c07f88aa089108e7d08f6ca
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-06-03 14:48:30 +0200

    Update salesforce_performance.sql

--- models/reporting/salesforce_performance.sql
+++ models/reporting/salesforce_performance.sql
@@ -52,7 +52,8 @@ WITH office_data as
         SUM(COALESCE(workable_leads,0)) as workable_leads,
         COUNT(DISTINCT CASE WHEN market = '999 - Invalid' THEN lead_id END) as ooa_leads,
         SUM(COALESCE(net_sale_count,0)) as net_sale_count,
-        SUM(COALESCE(median_value_per_set::float,0)) as set_value
+        SUM(COALESCE(median_value_per_set::float,0)) as set_value,
+        SUM(COALESCE(gross_sale_count,0)) as gross_sale_count
         FROM filetered_data
         GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
         {% if not loop.last %}UNION ALL
