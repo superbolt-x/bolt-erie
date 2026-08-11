6d4790a502d7c6726bf2df367eb1654d009a5b9c
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-08-11 16:21:54 +0200

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -2,8 +2,7 @@
     alias = target.database + '_blended_performance'
 )}}
 
-(SELECT * FROM {{ ref('facebook_sub_sources') }})
-UNION ALL
+
 (SELECT * FROM {{ ref('googleads_sub_sources') }})
 UNION ALL
 (SELECT * FROM {{ ref('bingads_sub_sources') }})
